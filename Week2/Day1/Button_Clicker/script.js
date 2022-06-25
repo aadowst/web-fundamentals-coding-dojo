7df01a08ee6087cca58c3ea0de92964a894cf568
Adrian Dowst <aadowst@gmail.com>
2022-06-25 14:24:01 -0700

    Button Clicker Intial Commit

---
+++ Week2/Day1/Button_Clicker/script.js
@@ -0,0 +1,11 @@
+function logOut(word){
+    word.innerText = "Logout";
+}
+
+function remove(element){
+    element.remove();
+}
+
+function wasLiked(status){
+    status.innerText="Ninja was liked";
+}
